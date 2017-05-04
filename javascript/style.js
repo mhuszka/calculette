f07e22e29875ab728be43b826ae41d96833a082b
Marine Belin <marinebelin.39@gmail.com>
2017-05-04 14:50:55 +0200

    javascript

--- javascript/style.js
+++ javascript/style.js
@@ -1,13 +1,46 @@
+
+// CLAVIER
+
+// création du clavier 
+
 var clavier = document.querySelector(".clavier");
 
 for(char of "123456789-+x/"){
 
-    clavier.innerHTML += "<div class='touche'>" + char + "</div>";  
+    clavier.innerHTML += "<div class='touche'>" + char + "</div>";   
 }
+var equal = document.createElement("button")
+equal.setAttribute('class', 'egal');
+document.body.querySelector(".clavier").appendChild(equal);
+
+// CALCULATRICE
+
+// variables
 
 var chiffreClavier = document.querySelectorAll(".touche");
+var input = document.querySelector("input");
+
+// évènements
 
 for(i=0; i<chiffreClavier.length; i++) {
 
-    chiffreClavier[i].addEventListener("click", lettreChoisie);
+    chiffreClavier[i].addEventListener("click", chiffreChoisi);
+}
+
+// déclaration d'une fonction
+
+function chiffreChoisi() {
+    
+    var chiffreValeur = this.innerHTML;
+    input.value += chiffreValeur;
+}
+
+var btnEgal = document.querySelector(".egal");
+console.log(btnEgal);
+
+btnEgal.addEventListener('click', afficheResultat);
+
+function afficheResultat() {
+    
+    input.value += eval(input.value);   
 }
